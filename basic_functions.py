8bf78db583dd5bc5d014f610486b7d9a688e834d
ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-04-29 14:09:07 -0600

    Co-authored-by: cinnamonflavoredbismuth <cinnamonflavoredbismuth@users.noreply.github.com> Co-authored-by: UCAS-LukeMurdock <UCAS-LukeMurdock@users.noreply.github.com> Co-authored-by: Tate-masterofCHAOS <Tate-masterofCHAOS@users.noreply.github.com>

--- basic_functions.py
+++ basic_functions.py
@@ -7,3 +7,20 @@ def btn(dict):
         else: 
             pygame.draw.rect(screen,dict['main_color'],[dict['StartPos']['x'],dict['StartPos']['y'],dict['width'],dict['height']]) # If mouse is not touching
         screen.blit(pygame.font.SysFont(dict['font'],dict['fontsize']).render(dict['text'] , True , dict["text_color"]),(dict['StartPos']['x']+dict["text_offset"],dict['StartPos']['y'])) # Putting text on the button
+
+
+def user_input():
+    pass
+
+EXAMPLE_DICT = {
+"width" : 500, # width of the button
+"height" : 50, # height of the button
+"StartPos": {"x" :  325,"y" : 630}, # Top left is 0,0
+"text": "Quit", 
+"font": "Arial",
+"fontsize": 35,
+"hover_color": (80,80,80),
+"main_color": (40,40,40),
+"text_offset": 225,
+"text_color": (255,255,255)
+}
