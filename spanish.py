c4d34f92fba4fdba44f292d914289c31ec117bd7
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-05-09 13:42:01 -0600

    Leaderboard

--- spanish.py
+++ spanish.py
@@ -2,14 +2,8 @@ import pygame
 import csv
 import random
 from unit import unit_select
-<<<<<<< HEAD
 from basic_functions import *
-
-=======
-from basic_functions import btn, display
-from basic_functions import click
-from misc import display_streaks
->>>>>>> c33cff1d01d89f7dc731f86ef46874d6a179c1d0
+from account_handler import leaderboard
 # Define the Spanish or Vanish game
 # This is a simple game where the user selects the correct answer from multiple options.
 # The game will display a lesson and multiple options, and the user has to select the correct one.
@@ -18,17 +12,7 @@ def lessons(acc):
     while True:
         
         # Initialize Pygame
-<<<<<<< HEAD
         pystart()
-=======
-        pygame.init()
-
-        # Set up the display
-        screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption('Spanish or Vanish')
-        pygame.display.set_icon(pygame.image.load('logo_uwu.png'))
-        #pygame.display.set_caption('Spanish or Vanish')
->>>>>>> c33cff1d01d89f7dc731f86ef46874d6a179c1d0
 
 
         # Image background
@@ -43,6 +27,7 @@ def lessons(acc):
         Quit_btn = button(500, 50, {"x" :  325,"y" : 630},"Quit", "Arial", 35, (80,80,80), (40,40,40), 225, 0, (255,255,255))
         Start_btn = button(500, 50, {"x" :  325,"y" : 530},"Start", "Arial", 35, (80,80,80), (40,40,40), 215, 0, (255,255,255))
 
+        Board = button(500, 50, {"x" :  325,"y" : 430},"Log In", "Arial", 35, (80,80,80), (40,40,40), 200, 0, (255,255,255))
 
 
         # Load lessons and questions from CSV
@@ -65,26 +50,26 @@ def lessons(acc):
             # Display the options
             btn(Quit_btn)
             btn(Start_btn)
-            # display_streaks(acc)
+
+            surface = font.render(f"{acc.name}'s Streak: {acc.streak}", True, (0, 0, 0))
+            screen.blit(surface, (450, 50))
+
+            # pygame.draw.rect(screen,Board.main_color,[Board.StartPos['x'],Board.StartPos['y'],Board.width,Board.height]) # If mouse is not touching
+            # "Streak Leaderboard"
+            ranked_streaks = leaderboard()
+            for rank, streak in enumerate(ranked_streaks[:5]):
+                surface = font.render(f"{rank+1}. {streak[1]} - {streak[0]} days", True, (0, 0, 0))
+                screen.blit(surface, (450, 150 + (rank*50)))
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-<<<<<<< HEAD
                     if if_clicked(Quit_btn,event) == True: # If  quit button clicked
                         # Go back to the main menu
                         running = False
 
                     elif if_clicked(Start_btn,event) == True: # If Start button clicked
-=======
-                    if Quit_btn['StartPos']['x'] <= event.pos[0] <= Quit_btn['StartPos']['x'] + Quit_btn['width'] and Quit_btn['StartPos']['y'] <= event.pos[1] <= Quit_btn['StartPos']['y'] + Quit_btn['height']:
-                        click()
-                        # Go back to the main menu
-                        running = False
-
-                    elif Start_btn['StartPos']['x'] <= event.pos[0] <= Start_btn['StartPos']['x'] + Start_btn['width'] and Start_btn['StartPos']['y'] <= event.pos[1] <= Start_btn['StartPos']['y'] + Start_btn['height']:
-                        click()
->>>>>>> c33cff1d01d89f7dc731f86ef46874d6a179c1d0
                         unit_select()
 
                     
@@ -96,14 +81,5 @@ def lessons(acc):
             pygame.time.delay(100)  # Delay to control frame rate
 
         # End of the game loop
-<<<<<<< HEAD
-        final_message = "Returning to Main Menu!"
-        final_surface = font.render(final_message, True, (0, 0, 0))
-        clear()
-        screen.blit(final_surface, (50, 50))
-        pygame.display.flip()  # Update the display
-        pygame.time.delay(2000)  # Wait for 2 seconds before quitting
-=======
         display("Returning to Main Menu!",2)
->>>>>>> c33cff1d01d89f7dc731f86ef46874d6a179c1d0
         break
