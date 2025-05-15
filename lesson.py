24238a1f21ed9f840fcc96fa4d7020956befe08e
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-05-14 20:44:59 -0600

    eeee

--- lesson.py
+++ lesson.py
@@ -302,5 +302,5 @@ def lesson(unit, _lesson, correct, incorrect,acc): # Lets the user do a lesson
     display("Thank you for playing!", 0, 450, 100)
 
 # FOR TESTING
-acc=load('cecily')
-print(lesson('Basics',' 1',[],[],acc))
+#acc=load('cecily')
+#print(lesson('Basics',' 1',[],[],acc))
